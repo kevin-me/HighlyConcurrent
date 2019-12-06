e7b9c979c58151aab2205f340e3adbfd92176f09
Crazy_kevin <kevin-me@outlook.com>
2019-12-06 16:19:04 +0800

    分治法 fork join

---
+++ src/ForkJoin/ForkJoin.java
@@ -0,0 +1,23 @@
+package ForkJoin;
+
+public class ForkJoin
+{
+    public static void main(String[] args)
+    {
+        /*
+          if(problem.size()>DEFAULT_SIZE){
+            childTask1 = new Task();
+            childTask2 = new Task();
+            childTask1.fork();
+            childTask2.fork();
+            childTaskResult1=childTask1.join();
+            childTaskResult2=childTask2.join();
+            taskResult=makeResults(childTaskResult1,childTaskResult2);
+            return taskResult;
+          }else{
+            taskResult=solveNasicProblem();
+            return taskResult;
+          }
+         */
+    }
+}
